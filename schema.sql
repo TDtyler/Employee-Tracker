cd81415df02f3d0648c5522c0b027510ce0ca641
TDTX 1 <tdtx1@TDTXs-MacBook-Pro.local>
2023-06-08 16:37:53 -0700

    created tables

---
+++ schema.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id varchar(60) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  is_manager BOOLEAN NOT NULL,
+  manager_id INT 
+  PRIMARY KEY(id)
+);
